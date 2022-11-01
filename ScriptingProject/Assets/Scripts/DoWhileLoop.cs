4fa82e8f9ae0bb8f80c8ac61927f920f810d6265
F00l29 <608564@student.dusd.net>
2022-11-01 11:37:10 -0700

    created scene and scripts
    created scene and scripts.

---
+++ ScriptingProject/Assets/Scripts/DoWhileLoop.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoWhileLoop : MonoBehaviour
+{
+    void Start()
+    {
+        bool shouldContinue = false;
+
+        do
+        {
+            print("Hello World");
+        }
+        while (shouldContinue == true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
