484125ec62278ef7172b7747d38f91d1854d0978
F00l29 <608564@student.dusd.net>
2022-11-14 10:34:53 -0800

    AwakeAndStart
    Added the AwakeAndStart Scene and script

---
+++ ScriptingProject/Assets/Scripts/AwakeAndStart.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour
+{
+    
+    void Awake()
+    {
+        Debug.Log("Awake ccalled.");
+    }
+
+    
+    void Start()
+    {
+        Debug.Log("Start called.");
+    }
+}
